cb43eda2143d021a530489c62c5be0b1f3a55a95
ANS1233 <anass4arabi@gmail.com>
2024-10-28 11:51:05 +0100

    igw

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
 region = "eu-west-3"
 }
 
-
 variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
@@ -21,6 +20,25 @@ resource "aws_subnet" "myapp-subnet-1" {
     availability_zone = var.avail_zone
     tags = {
         Name: "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = 
+    }
+    tags = {
+        Name: "${var.env_prefix}-rtb"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+    
+    tags = {
+        Name: "${var.env_prefix}-igw"
     }
 }
 
