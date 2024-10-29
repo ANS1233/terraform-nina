72e7f2abd39932261a128a3c7dd872dd2d885721
ANS1233 <anass4arabi@gmail.com>
2024-10-29 18:38:47 +0100

    29-10

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable avail_zone {}
 variable env_prefix {}
+variable instance_type {}
+variable "key_name" {}
+  
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_blocks
@@ -41,4 +44,91 @@ resource "aws_internet_gateway" "myapp-igw" {
         Name: "${var.env_prefix}-igw"
     }
 }
+resource "aws_route_table_association" "a-rtb-association" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["88.178.210.35/32"]
 
+  }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = {
+        Name: "${var.env_prefix}-sg"
+    }
+
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+
+most_recent = true
+owners = ["amazon"]
+filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+}
+filter {
+ name = "virtualization-type"
+    values = ["hvm"]   
+}
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+
+}
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+
+}
+resource "tls_private_key" "rsa-4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa-4096.public_key_openssh
+}
+
+resource "local_file" "private_key"{
+    content = tls_private_key.rsa-4096.private_key_pem
+    filename = var.key_name
+}
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+
+    key_name = aws_key_pair.key_pair.key_name
+    user_data = file("entry-script.sh")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+}
+
+
+
+    
